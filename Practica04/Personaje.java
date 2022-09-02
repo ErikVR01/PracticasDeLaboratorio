9b609b7f06b4f04c999f187e5e4cea2d371f70bf
ErikVR01 <111620924+ErikVR01@users.noreply.github.com>
2022-09-02 11:06:34 -0500

    Practica04 11:06

---
+++ Practica04/Personaje.java
@@ -0,0 +1,42 @@
+public class Personaje{
+	private String nombre;
+	private int edad;
+
+	public Personaje(String nombre){
+		this.nombre=nombre;
+		this.edad=0;
+	}
+
+	public int getEdad(){
+		return edad;
+	}
+
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+
+	}
+
+	public String getNombre(){
+		return nombre;
+	}
+
+	public void saludar(){
+		System.out.println("Hola Alumno de POO " +nombre);
+	}
+
+
+	public boolean setEdad(int edad){
+		if (edad>0) {
+			if (edad<120){
+			
+				this.edad=edad;
+				return true;
+
+			}else{
+				
+				return false;
+				
+			}
+		}return false;
+	}
+}
