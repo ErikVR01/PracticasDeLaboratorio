0ea309a896d49339106d8889e6bd1e822218b7c4
ErikVR01 <111620924+ErikVR01@users.noreply.github.com>
2022-09-09 10:45:16 -0500

    Practica06 10:45

--- Practica06/proyecto/src/edu/escuela/gamepz/pruebas/PruebaPersonaje.java
+++ Practica06/proyecto/src/edu/escuela/gamepz/pruebas/PruebaPersonaje.java
@@ -2,20 +2,17 @@ package edu.escuela.gamepz.pruebas;
 import edu.escuela.gamepz.personajes.*;
 public class PruebaPersonaje{
 	public static void main(String[] args) {
-		Personaje alumno= new Personaje("Kal El");
-		alumno.saludar();
-		System.out.println("Nombre: "+alumno.getNombre()+ " Edad: " +alumno.getEdad());
-		System.out.println("Modificando el nombre "+alumno.getNombre());
-		alumno.setNombre("Michael Jordan");
-		alumno.saludar();
-		if (!alumno.setEdad(30)){
-			System.out.println("Edad sin cambios");
-		}
-		System.out.println("Nombre: "+alumno.getNombre()+ " Edad: "+alumno.getEdad());
-		System.out.println("Modificando edad en 130");
-		if (!alumno.setEdad(130)){
-			System.out.println("Edad sin cambio");
-		}
-		System.out.println("Nombre: "+alumno.getNombre()+ " Edad: "+alumno.getEdad());
+		Personaje per01 = new Personaje("Almendra", 100)
+		Personaje per02 = new Personaje("Ricardo", 10)
+		Personaje per03 = new Personaje("Bianca", 1)
+		Personaje per04 = new Personaje("Leonel", -10)
+		Personaje per05 = new Personaje("Hector", 500)
+
+		System.out.println(per01.getDetalle());
+		System.out.println(per02.getDetalle());
+		System.out.println(per03.getDetalle());
+		System.out.println(per04.getDetalle());
+		System.out.println(per05.getDetalle());
+
 	}
 }
