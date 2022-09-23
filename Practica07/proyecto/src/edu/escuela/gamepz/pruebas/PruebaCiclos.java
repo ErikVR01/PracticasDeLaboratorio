dffcb963938eebf012196cf467081a3098b93044
ErikVR01 <111620924+ErikVR01@users.noreply.github.com>
2022-09-23 11:45:18 -0500

    Practica 07 11:45

--- Practica07/proyecto/src/edu/escuela/gamepz/pruebas/PruebaCiclos.java
+++ Practica07/proyecto/src/edu/escuela/gamepz/pruebas/PruebaCiclos.java
@@ -1,15 +1,21 @@
-Public class PruebaCiclos{
-	Personaje per01 = new Personaje("Diego");
-	Personaje per01 = new Personaje("Almendra");
-	Personaje per01 = new Personaje("Bianca");
-	Personaje per01 = new Personaje("Jordan");
-	
-	int n=0;
-	int m=0;
-	while (n >= m){
-		n=random.nextInt(0 - 100 + 1)
-		m=random.nextInt(0 - 100 + 1)
-		System.out.println(n);
-		System.out.println(m);
+package edu.escuela.gamepz.pruebas;
+import edu.escuela.gamepz.personajes.*;
+public class PruebaCiclos{
+	public static void main(String[] args) {
+		Personaje per01 = new Personaje("Diego");
+		Personaje per02 = new Personaje("Almendra");
+		Personaje per03 = new Personaje("Bianca");
+		Personaje per04 = new Personaje("Jordan");
+		
+		int n=0;
+		int m=0;
+		While (n >= m);{
+			n=random.nextInt(0 - 100 + 1);
+			m=random.nextInt(0 - 100 + 1);
+			System.out.println(n);
+			System.out.println(m);
+		}
+			
 	}
+		
 }
