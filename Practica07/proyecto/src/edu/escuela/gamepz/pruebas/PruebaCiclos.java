fedd39597b090c69d1b5f6c3962ed4338300f382
ErikVR01 <111620924+ErikVR01@users.noreply.github.com>
2022-09-23 08:26:43 -0500

    Practica 07 8:26

---
+++ Practica07/proyecto/src/edu/escuela/gamepz/pruebas/PruebaCiclos.java
@@ -0,0 +1,6 @@
+Public class PruebaCiclos{
+	Personaje per01 = new Personaje("Diego");
+	Personaje per01 = new Personaje("Almendra");
+	Personaje per01 = new Personaje("Bianca");
+	Personaje per01 = new Personaje("Jordan");
+}
