64f6aadfcf4e16b7545ce17b1bc84c1d58850c72
ErikVR01 <111620924+ErikVR01@users.noreply.github.com>
2022-10-07 08:10:13 -0500

    practica 08 8:10

---
+++ Practica08/proyecto/src/edu/escuela/gamepz/personajes/Personaje.java
@@ -0,0 +1,50 @@
+package edu.escuela.gamepz.personajes;
+public class Personaje{
+	private String nombre;
+	private int vida;
+
+	public Personaje(String nombre){
+		this.nombre=nombre;
+
+	}
+	
+	public int getVida(){
+		return edad;
+	}
+
+	public void setNombre(String nombre){
+		if (nombre.length()>5 && nombre.length()<25){
+			this.nombre = nombre;
+		}
+	}
+
+	public String getDetalle(){
+		return nombre + "\t" + edad;
+	}
+
+	
+
+	public String getNombre(){
+		return nombre;
+	}
+
+	public void saludar(){
+		System.out.println("Hola Alumno de POO " +nombre);
+	}
+
+
+	public boolean setEdad(int edad){
+		if (edad>0) {
+			if (edad<120){
+			
+				this.edad=edad;
+				return true;
+
+			}else{
+				
+				return false;
+
+			}
+		}return false;
+	}
+}
