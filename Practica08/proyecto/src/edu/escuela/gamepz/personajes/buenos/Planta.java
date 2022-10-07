cfb1aab9fbb6553d9b1f9f270550dd108c6c9f23
ErikVR01 <111620924+ErikVR01@users.noreply.github.com>
2022-10-07 10:19:53 -0500

    practica 08 10:19

--- Practica08/proyecto/src/edu/escuela/gamepz/personajes/buenos/Planta.java
+++ Practica08/proyecto/src/edu/escuela/gamepz/personajes/buenos/Planta.java
@@ -4,4 +4,26 @@ public class Planta extends Personaje{
     public char getEscudo(){
         return escudo;
     }
+
+    public Planta(String nombre, int vida, char escudo){
+        super(nombre, vida);
+        this.escudo = escudo;
+    }
+
+    public Planta(String nombre, char escudo){
+        super(nombre, 3);
+        this.escudo = escudo;
+    }
+
+    public Planta(String nombre, int vida){
+        super(nombre, vida);
+        escudo = 'A';
+    }
+
+    public Planta(String nombre){
+        super(nombre, 3);
+        escudo = 'A';
+    }
+
+    
 }
