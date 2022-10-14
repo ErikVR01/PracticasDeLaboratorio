0c612d3afe35765f6ef6310baef90e2c4378f27f
ErikVR01 <111620924+ErikVR01@users.noreply.github.com>
2022-10-14 12:12:24 -0500

    Practica 09 12:12

--- Practica09/Proyecto/src/mx/desasof/inmobiliaria/prueba/PruebaCasa.java
+++ Practica09/Proyecto/src/mx/desasof/inmobiliaria/prueba/PruebaCasa.java
@@ -1,17 +1,38 @@
+package mx.desasof.inmobiliaria.prueba;
+import mx.desasof.inmobiliaria.casas.*;
 public class PruebaCasa{
 	public static void main(String[] args) {
-		Casa casas [] = new Casa [9];
+		Casa cas01 = new Casa("Estandar", 3, 3000000);
+		Campo camp01 = new Campo("Cazador", 3.5f, 10f);
+		Urbana urb01 = new Urbana("Residencia", 3, 2500000, 2);
+		Casa cas02 = new Casa("Estandar", 3, 3000000);
+		Campo camp02 = new Campo("Cazador", 3.5f, 10f);
+		Urbana urb02 = new Urbana("Residencia", 3, 2500000, 2);
+		Casa cas03 = new Casa("Estandar");
+		Campo camp03 = new Campo("Cazador", 4, 15000, 3f, 5f);
+		Urbana urb03 = new Urbana("Residencia", 5);
 
-		casas [0]= new Casa ("Estandar", 3, 3000000);
-		casas [1]= new Campo ("Cazador", 3.5f , 10f);
-		casas [2]= new Urbana ("Residencia", 3, 2500000, 2);
-		casas [3]= new Casa ("Estandar", 3, 3000000);
-		casas [4]= new Campo ("Cazador", 3.5f , 10f);
-		casas [5]= new Urbana ("Residencia", 3, 2500000, 2);
-		casas [6]= new Casa ("Estandar");
-		casas [7]= new Campo ("Cazador", 4, 15000, 3f , 5f);
-		casas [8]= new Urbana ("Residencia", 5);
+		Casa[] casas = {cas01, camp01, urb01, cas02, camp02, urb02, cas03, camp03, urb03};
 
-		int i = 0
+		int i = 0;
+
+		while (i < casas.length){
+			for (Casa c : casas) {
+				if (casas[i] == c) {
+					System.out.println(casas[i]+" son el mismo objeto " + c);
+					System.out.println("= = = = =");
+					break;
+				}else if (casas[i].equals(c)) {
+					System.out.println(casas[i]+ " son iguales " + c);
+					System.out.println("e e e e e");
+					break;
+				}else{
+					System.out.println(casas[i]+ " son diferentes "+ c);
+					System.out.println("! ! ! ! !");
+
+				}
+			}
+			i++;
+		}
 	}
 }
