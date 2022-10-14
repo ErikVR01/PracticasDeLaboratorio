8188a1e3d3dfa78ee26cc63969177906f4e8acba
ErikVR01 <111620924+ErikVR01@users.noreply.github.com>
2022-10-14 08:13:21 -0500

    Practica 09 8:13

--- Practica09/Proyecto/src/mx/desasof/practica09/casas/Casa.java
+++ Practica09/Proyecto/src/mx/desasof/practica09/casas/Casa.java
@@ -15,4 +15,27 @@ public class Casa{
 		costo=2000000;
 	}
 
+	public int hashcode(){
+		return tipo.hashcode()*hab*costo;
+	}
+
+
+	public boolean equals(Object o){
+		if (o != null && o instanceof Casa) {
+			Casa p= (Casa) o;
+
+			if (tipo.equals(p.tipo) && (hab == p.hab)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public String toString(){
+		return tipo + " " + hab + " " + costo;
+	}
+
+
+
+
 }
