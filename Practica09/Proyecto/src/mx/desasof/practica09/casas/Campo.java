be3a86403f2dc528b051253b2e8bac17366376d1
ErikVR01 <111620924+ErikVR01@users.noreply.github.com>
2022-10-14 08:52:09 -0500

    Practica 09 8:52

--- Practica09/Proyecto/src/mx/desasof/practica09/casas/Campo.java
+++ Practica09/Proyecto/src/mx/desasof/practica09/casas/Campo.java
@@ -1,1 +1,25 @@
-Campo
+public class Campo extends Casa{
+	
+	float piscina [2]= new piscina();
+
+	public Campo(String tipo, int hab, float costo, float largo, float ancho){
+		super(tipo, hab, costo);
+		ancho=0
+		largo=1
+
+	}
+
+	public int hashcode(){
+		super.hashcode()*(int)ancho+(int)largo/5;
+	}
+
+	public boolean equals(Object ob){
+		super.equals(ob) && ob instanceof Campo;
+	}
+
+	public String toString(){
+		return tipo + " " + hab + " " + costo;
+	}
+
+
+}
