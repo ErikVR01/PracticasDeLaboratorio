c640455a4aef62f9f583a428706c5f9a0bfa8e49
ErikVR01 <111620924+ErikVR01@users.noreply.github.com>
2022-11-04 12:07:52 -0600

    practica 12 12:07

--- practica12/src/edu/escuela/gamepz/pruebas/PruebaTablero.java
+++ practica12/src/edu/escuela/gamepz/pruebas/PruebaTablero.java
@@ -1,16 +1,21 @@
+package edu.escuela.gamepz.pruebas;
+import edu.escuela.gamepz.personajes.Personaje;
+import edu.escuela.gamepz.personajes.buenos.Planta;
+import edu.escuela.gamepz.personajes.malos.Zombie;
+import edu.escuela.gamepz.utils.*;
 public class PruebaTablero{
 	public static void main(String[] args) {
-		Personaje per01 = new Planta("David", 100);
-        Personaje per02 = new Zombie("Bianca");
-        Planta plan01 = new Planta("Fabian", 10, Escudo.MEDIO);
-        Planta plan02 = new Planta("Almendra", 50);
-        Planta plan03 = new Planta("Ricardo", Escudo.BAJO);
-        Planta plan04 = new Planta("Silvia");
-        Zombie zom01 = new Zombie("Armando", 80, false);
-        Zombie zom02 = new Zombie("Josseline", true);
-        Zombie zom03 = new Zombie("Eduardo");
-        Personaje[] datos = {per01,per02, plan01,plan02,plan03,plan04,zom01,zom02,zom03};
-
+		Personaje[]datos={
+		new Planta("David", 100),
+        new Zombie("Bianca"),
+        new Planta("Fabian", 10, Escudo.MEDIO),
+        new Planta("Almendra", 50),
+        new Planta("Ricardo", Escudo.BAJO),
+        new Planta("Silvia"),
+        new Zombie("Armando", 80, false),
+        new Zombie("Josseline", true),
+        new Zombie("Eduardo")
+    	};
         for (Personaje p : datos ) {
         	int pos = (int)(Math.random()*20-5);
         	try{
@@ -20,6 +25,18 @@ public class PruebaTablero{
         	}
         }
 
-        Tablero.mostrar()
+        Tablero.mostrar();
+
+        for (int i=0; i<Tablero.MAX_SIZE; i++) {
+        	int pos = (int)(Math.random()*20-5);
+
+        	try{
+        		Tablero.borrar(pos);
+        	}catch(PersException e){
+        		System.err.println(e.getMessage() + " " + e.getPos());
+        	}
+        }
+
+        Tablero.mostrar();
 	}
 }
