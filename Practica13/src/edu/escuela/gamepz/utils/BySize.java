7f65af7bc58f35005648857ddf88922b0bc3eba5
ErikVR01 <111620924+ErikVR01@users.noreply.github.com>
2022-11-11 12:00:10 -0600

    practica 13 12:00

--- Practica13/src/edu/escuela/gamepz/utils/BySize.java
+++ Practica13/src/edu/escuela/gamepz/utils/BySize.java
@@ -2,5 +2,16 @@ package edu.escuela.gamepz.utils;
 import java.util.Comparator;
 import edu.escuela.gamepz.personajes.Personaje;
 public class BySize implements Comparator <Personaje>{
-
+	public int compare(Personaje p1, Personaje p2){
+		if (p1.genSize() != p2.genSize()){
+            return (p2.genSize() > p1.genSize()) ? -1 : 1;
+        }
+        if (p1.getNombre().compareTo(p2.getNombre()) != 0){
+        	return (p1.getNombre().compareTo(p2.getNombre()));
+        }
+        if (p1.getVida() == p2.getVida()){
+        	return 0;
+        }
+        return p2.getVida() - p1.getVida();
+	}
 }
