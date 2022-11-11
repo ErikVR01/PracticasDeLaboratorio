f41eb580d79f5a44387463b0e271f4588b0c8d78
ErikVR01 <111620924+ErikVR01@users.noreply.github.com>
2022-11-11 12:15:06 -0600

    Practica 13 12:15

--- Practica13/src/edu/escuela/gamepz/pruebas/PruebaColeccion.java
+++ Practica13/src/edu/escuela/gamepz/pruebas/PruebaColeccion.java
@@ -1,3 +1,27 @@
+package edu.escuela.gamepz.pruebas;
+
+
+import edu.escuela.gamepz.personajes.Personaje;
+import edu.escuela.gamepz.personajes.buenos.Planta;
+import edu.escuela.gamepz.personajes.malos.Zombie;
+import edu.escuela.gamepz.utils.BySize;
+import edu.escuela.gamepz.utils.ByVida;
+import edu.escuela.gamepz.utils.Escudo;
+import edu.escuela.gamepz.utils.Tablero;
 public class PruebaColeccion{
-	
+	public static void main(String[] args) {
+		Personaje[] datos = {
+            new Planta("Fabian", Tablero.genVida()), 
+            new Zombie("Bianca",Tablero.genVida()),
+            new Planta("Fabian", Tablero.genVida(), Escudo.MEDIO),
+            new Planta("Armando", Tablero.genVida()),
+            new Planta("Bianca", Tablero.genVida(),Escudo.BAJO),
+            new Planta("Dayan",Tablero.genVida()),
+            new Zombie("Armando", Tablero.genVida(), false),
+            new Zombie("Dayan",Tablero.genVida(), true),
+            new Zombie("Armando",Tablero.genVida())
+		};
+		TreeSet<Personaje> ts = new TreeSet<>();
+		LinkedList<Personaje> linked = new LinkedList<>();
+	}
 }
