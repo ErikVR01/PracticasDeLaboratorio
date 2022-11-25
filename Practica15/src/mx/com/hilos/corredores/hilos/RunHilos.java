4ddaa828fe871a59ee857781bb6324bff64b2ac8
ErikVR01 <111620924+ErikVR01@users.noreply.github.com>
2022-11-25 11:05:36 -0600

    practica 15 11:05

--- Practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
+++ Practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
@@ -2,5 +2,6 @@ public class RunHilos{
 	public static void iniciar(JTextField txt1, JTextField txt2, JLabel elem1, JLabel elem2){
 		char ch1 = txt1.getText().charAt(0);
 		char ch2 = txt2.getText().charAt(0);
+		
 	}
 }
