d38c83318c5975772273bcf1aee9d0435203d848
ErikVR01 <111620924+ErikVR01@users.noreply.github.com>
2022-11-25 11:54:54 -0600

    practica 15 11:54

---
+++ Practica15/src/mx/com/hilos/corredores/hilos/Letra.java
@@ -0,0 +1,28 @@
+package mx.com.hilos.corredores.hilos;
+import javax.swing.JLabel;
+public class Letra implements Runnable{
+
+	public char letra;
+	private JLabel etq;
+
+	public Letra(char letra,JLabel etq){
+        this.letra=letra;
+        this.etq=etq;
+    }
+
+    public void run() {
+        String cadena =etq.getText();
+        for (int i = 0; i <= 50; i++) {
+            cadena+=letra;
+            etq.setText(cadena);
+            int rand=(int)Math.random()*1000;
+            try {
+                Thread.sleep(rand);
+            } catch (InterruptedException e) {
+        }
+        cadena+="Finalizado";
+        etq.setText(cadena);
+        }
+    } 
+
+}
