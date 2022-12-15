ec5de98e962103b77692848943f1c5040df38734
ErikVR01 <111620924+ErikVR01@users.noreply.github.com>
2022-12-15 09:07:29 -0600

    Extraordinario 9:07

--- Extraordinario/src/mx/edu/elextra/extraeval/dispositivos/Celular.java
+++ Extraordinario/src/mx/edu/elextra/extraeval/dispositivos/Celular.java
@@ -1,6 +1,13 @@
-public class Celular{
+package mx.edu.elextra.extraeval.dispositivos;
+public class Celular extends Dispositivo{
 	float procesador;
-	public Celular(String float float){
-
+	public Celular(String marca, float costo, float procesador){
+		super(marca, costo);
+		this.procesador=procesador;
 	}
+
+	public float getProcesador(){
+		return procesador;
+	}
+
 }
--- Extraordinario/src/mx/edu/elextra/extraeval/dispositivos/Computadora.java
+++ Extraordinario/src/mx/edu/elextra/extraeval/dispositivos/Computadora.java
@@ -1,6 +1,18 @@
-public class Computadora{
-	int ram;
-	public Computadora(String float int){
-
+package mx.edu.elextra.extraeval.dispositivos;
+public class Computadora extends Dispositivo{
+	private int ram;
+	public Computadora(String marca, float costo, int ram){
+		super(marca, costo);
+		this.ram=ram;
 	}
+
+	public String toString(){
+		return super.toString() + '\t' + ram;
+	}
+
+	public int getRam(){
+		return ram;
+	}
+
+
 }
