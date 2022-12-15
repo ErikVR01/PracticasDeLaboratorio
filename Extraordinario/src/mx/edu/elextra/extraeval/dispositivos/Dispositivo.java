82b69937306038e5589f22ae876536b5f132f45e
ErikVR01 <111620924+ErikVR01@users.noreply.github.com>
2022-12-15 09:00:06 -0600

    Extraordinario 9:00

---
+++ Extraordinario/src/mx/edu/elextra/extraeval/dispositivos/Dispositivo.java
@@ -0,0 +1,33 @@
+package mx.edu.elextra.extraeval.dispositivos;
+public class Dispositivo implements Comparable<Dispositivo> {
+
+	private String marca;
+	private float costo;
+
+	public Dispositivo(String marca, float costo){
+		this.marca=marca;
+		this.costo=costo;
+	}
+
+	public String toString(){
+		return marca + '\t' + costo;
+	}
+
+	public String getMarca(){
+		return marca;
+	}
+
+	public float getCosto(){
+		return costo;
+	}
+	public int compareTo(Dispositivo o) {
+        if(this.marca.compareTo(o.marca) !=0){
+			return this.marca.compareTo(o.marca);
+		}
+		if(this.costo == o.costo){
+			return 0;
+		}
+		return (o.costo < this.costo)? -1 : 1;
+    }
+
+}
